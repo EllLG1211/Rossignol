1c83b5fcff22213efbbbe101a5177d5a940d246e
Elliott Le Guéhennec <elliott.leguehennec@gmail.com>
2022-09-16 12:04:22 +0200

    Created a class and a test suite for test purposes

---
+++ Sources/Model/ProprietaryEntry.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class ProprietaryEntry : Entry
+    {
+        protected long Uid { get; init; }
+
+        public string Login { get; set; }
+
+        protected string Password { get; set; }
+
+        public string Website { get; set; }
+
+        public string Note { get; set; }
+
+        public ProprietaryEntry(long uid, string login, string password, string website, string note)
+        {
+            Uid = uid;
+            Login = login;
+            Password = password;
+            Website = website;
+            Note = note ?? string.Empty;
+        }
+
+        public ProprietaryEntry(long uid, string login, string password, string website)
+            : this(uid, login, password, website, string.Empty) { }
+    }
+}
---
+++ Sources/Tests/Model_Tests/ProprietaryEntry_Tests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Model;
+
+namespace Model_Tests
+{
+    public class ProprietaryEntry_Tests
+    {
+        [Theory]
+        [InlineData("schtroumpf", "truc", "abracadabra", true)]
+        [InlineData("lorem.ipsum@mail.com", "Lorem Ipsum", "abracadabra", true)]
+        [InlineData("schtroumpf", "truc", null, false)]
+        [InlineData("korè@bidule", "Wikipédia", "abracadabra", true)]
+        public void Constructor_ShouldAssignValues(string login, string website, string note, bool noteSuccessExpected)
+        {
+            ProprietaryEntry entry = new(42, login, "lorem ipsum", website, note);
+            Assert.Equal(login, entry.Login);
+            Assert.Equal(website, entry.Website);
+            if (noteSuccessExpected) Assert.Equal(note, entry.Note);
+            else Assert.NotEqual(note, entry.Note);
+        }
+
+        [Fact]
+        public void Constructor_ShouldReassignNote()
+        {
+            ProprietaryEntry entry = new(42, "loremipsum@gmail.com", "rickroll", "Discord");
+            Assert.Equal(string.Empty, entry.Note);
+        }
+
+        /*[Fact]
+        public void Label_ShouldReturnLoginAndWebsite()
+        {
+            ProprietaryEntry entry = new(42, "loremipsum@gmail.com", "rickroll", "Discord");
+            Assert.Equal("Discord - loremipsum@gmail.com", entry.Label);
+        }*/
+    }
+}

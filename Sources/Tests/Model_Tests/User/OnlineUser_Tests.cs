1b7b65700ab19e41c7ef7695dac916985077147f
yorick.geoffre <yorick.geoffre@etu.uca.fr>
2022-09-28 10:47:46 +0200

    fixed comparaison between mails

--- Sources/Tests/Model_Tests/User/OnlineUser_Tests.cs
+++ Sources/Tests/Model_Tests/User/OnlineUser_Tests.cs
@@ -5,13 +5,33 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 using Model.User;
+using Model;
+using System.Net.Mail;
 
 namespace Model_Tests.User
 {
     public class OnlineUser_Tests
     {
+        [Fact]
+        public void EmailSetter_ShouldAssignValue()
+        {
+            AbstractUser user = new OnlineUser("kaman.zok@organs.com");
+            Assert.Equal("kaman.zok@organs.com", user.email.Address);
+        }
+
+        [Theory]
+        [InlineData(true, "sarmat@torba.com", "sarmat@torba.com")]
+        [InlineData(true, "Sarmat@torba.com", "sarmat@torba.com")]
+        [InlineData(false, "sortob@torba.com", "sarmat@torba.com")]
+        public void UserEquality_ShouldBeEqual(bool shouldBeEqual, string email1, string email2)
+        {
+            AbstractUser user1 = new OnlineUser(email1);
+            AbstractUser user2 = new OnlineUser(email2);
+            Assert.Equal(shouldBeEqual, (((IEquatable<AbstractUser>)user1).Equals(user2)));
+        }
+
         /// <summary>
-        /// Test if class constructor set the correct values.
+        /// Test if class constructor throws an exception or not
         /// </summary>
         /// <param name="login"></param>
         /// <param name="app"></param>

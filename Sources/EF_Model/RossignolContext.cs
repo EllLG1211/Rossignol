0a16bd5ad3828f3cce7d92466dc35ef6a1737c07
Valentin Clergue <valentin.clergue@etu.uca.fr>
2022-09-30 15:19:54 +0200

    First steps with EF

---
+++ Sources/EF_Model/RossignolContext.cs
@@ -0,0 +1,13 @@
+﻿using EF_Model.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_Model
+{
+    public class RossignolContext : DbContext
+    {
+        public DbSet<ProprietaryEntryEntity> ProprietaryEntries { get; set; } 
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source=Rossignol.bd");
+    }
+}
---
+++ Sources/EF_Model/Utils/ToProprietaryEntryEntity.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EF_Model.Utils
+{
+    public static class ToProprietaryEntryEntity
+    {
+    }
+}

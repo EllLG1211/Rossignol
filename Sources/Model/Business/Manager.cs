3e3ce430ad1f61bb9b68d7b4507ce141e101f035
Mathis Ribemont <mathis.ribemont@etu.uca.fr>
2022-10-06 09:54:10 +0200

    #14 add test manager, but test failed

--- Sources/Model/Business/Manager.cs
+++ Sources/Model/Business/Manager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model.Business.Users;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,43 @@ namespace Model.Business
 {
     public class Manager
     {
+        public AbstractUser ConnectedUser { get; private set; }
 
+        /*public void Login(string mail, string password)
+        {
+
+        }*/
+
+        /// <summary>
+        /// Register the user for <i>ConnectedUser</i>.
+        /// </summary>
+        /// <param name="mail"></param>
+        /// <param name="password"></param>
+        /// <param name="confirmPassword"></param>
+        /// <exception cref="ArgumentNullException">Throwed when one of arguments is null.
+        ///     The exception contains name of the null field (<i>mail</i>, <i>password</i>, <i>confirmPassword</i>).
+        /// </exception>
+        /// <exception cref="ArgumentException">Throwed when <i>password</i> and <i>confirmPassword</i> are not equal.</exception>
+        public void Signin(string mail, string password, string confirmPassword)
+        {
+            if(String.IsNullOrEmpty(mail))
+            {
+                throw new ArgumentNullException("mail");
+            }
+            else if(String.IsNullOrEmpty(password))
+            {
+                throw new ArgumentNullException("password");
+            }
+            else if(String.IsNullOrEmpty(confirmPassword)) 
+            { 
+                throw new ArgumentNullException("confirmPassword"); 
+            }
+            else if(!password.Equals(confirmPassword))
+            {
+                throw new ArgumentException("Password does not equal confirmPassword");
+            }
+
+            ConnectedUser = new ConnectedUser(mail, password);
+        }
     }
 }
---
+++ Sources/Tests/Model_Tests/Business/Manager_Tests.cs
@@ -0,0 +1,71 @@
+﻿using Model.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Model_Tests.Business
+{
+    public class Manager_Tests
+    {
+        [Theory]
+        [InlineData(true, null, "1234", "1234")]
+        [InlineData(true, "test@test.com", null, "1234")]
+        [InlineData(true, "test@test.com", "1234", null)]
+        [InlineData(true, "", "1234", "1234")]
+        [InlineData(true, "test@test.com", "", "1234")]
+        [InlineData(true, "test@test.com", "1234", "")]
+        [InlineData(false, "test@test.com", "1234", "1234")]
+        [InlineData(false, "test@test.com", "1234", "12345")]
+        public void Signin_ShouldThrowArgumentNullException(bool expected, string mail, string password, string confirmPassword)
+        {
+            Manager manager = new Manager();
+            if (expected)
+            {
+                Assert.Throws<ArgumentNullException>(() => { manager.Signin(mail, password, confirmPassword); });
+                return;
+            } else
+            {
+                manager.Signin(mail, password, confirmPassword);
+                Assert.False(expected);
+            }
+        }
+
+        [Theory]
+        [InlineData(false, null, "1234", "1234")]
+        [InlineData(false, "test@test.com", null, "1234")]
+        [InlineData(false, "test@test.com", "1234", null)]
+        [InlineData(false, "", "1234", "1234")]
+        [InlineData(false, "test@test.com", "", "1234")]
+        [InlineData(false, "test@test.com", "1234", "")]
+        [InlineData(false, "test@test.com", "1234", "1234")]
+        [InlineData(true, "test@test.com", "1234", "12345")]
+        public void Signin_ShouldThrowArgumentException(bool expected, string mail, string password, string confirmPassword)
+        {
+            Manager manager = new Manager();
+            try
+            {
+                manager.Signin(mail, password, confirmPassword);
+            }
+            catch (ArgumentException e)
+            {
+                Assert.True(expected);
+            }
+            catch (Exception e)
+            {
+                Assert.False(expected);
+            }
+            Assert.False(expected);
+        }
+
+        [Fact]
+        public void Signin_ShouldInstantiateConnectedUserInManager()
+        {
+            Manager manager = new Manager();
+            manager.Signin("test@test.com", "1234", "1234");
+            Assert.NotNull(manager.ConnectedUser);
+        }
+    }
+}

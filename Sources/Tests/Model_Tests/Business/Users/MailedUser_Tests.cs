af55e0b03829a4402c0706f0d24a9efaf0d16235
Mathis Ribemont <mathis.ribemont@etu.uca.fr>
2022-10-23 20:58:49 +0200

    #44 add MailedUser tests

---
+++ Sources/Tests/Model_Tests/Business/Users/MailedUser_Tests.cs
@@ -0,0 +1,53 @@
+﻿using Model.Business.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Model_Tests.Business.Users
+{
+    public class MailedUser_Tests
+    {
+        [Fact]
+        public void Equals_ShouldReturnFalseIfOtherIsNull()
+        {
+            MailedUser user = new ReadOnlyUser("test", "1234");
+            Assert.False(user.Equals(null));
+        }
+
+        [Theory]
+        [MemberData(nameof(Equals_ShouldReturnTrue_Data))]
+        public void Equals_ShouldReturnTrue(bool expected, MailedUser user, object other)
+        {
+            Assert.True(user.Equals(other) == expected);
+        }
+
+        public static IEnumerable<Object[]> Equals_ShouldReturnTrue_Data()
+        {
+            MailedUser user = new ConnectedUser("test", "1234");
+
+            yield return new object[]
+            {
+                true,
+                user,
+                user
+            };
+
+            yield return new object[]
+            {
+                true,
+                user,
+                new ConnectedUser("test", "12234")
+            };
+
+            yield return new object[]
+            {
+                false,
+                user,
+                new ConnectedUser("tedst", "12234")
+            };
+        }
+    }
+}

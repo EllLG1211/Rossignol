8b03be6bb0cf6f0764599a942ec6e703735853cd
Onyx ab3q <Onyxab3q@outlook.com>
2022-10-23 22:25:27 +0200

    modified ef test

--- Sources/EF_Model/Managers/EntryEntityManager.cs
+++ Sources/EF_Model/Managers/EntryEntityManager.cs
@@ -1,6 +1,7 @@
 ﻿using EF_Model.Entities;
 using EF_Model.Utils;
 using EncryptedModel.Business.Entries;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,25 +12,33 @@ namespace EF_Model.Managers
 {
     public class EntryEntityManager
     {
-        public static async Task addEntry(EncryptedProprietaryEntry e, LocalUserEntity lue)
+
+        public static async Task addEntry(EncryptedProprietaryEntry e, LocalUserEntity lue, DbContextOptions<RossignolContextLocal> options)
         {
-            using (var context = new RossignolContextLocal())
+            using (var context = new RossignolContextLocal(options))
             {
                 context.EncryptedEntriesSet.Add(e.ToEntity(lue));
                 context.SaveChanges();
             }
         }
 
-        public static async Task removeEntry(EncryptedProprietaryEntry e, LocalUserEntity lue)
+        public static async Task removeEntry(EncryptedProprietaryEntry e, LocalUserEntity lue, DbContextOptions<RossignolContextLocal> options)
         {
-            using (var context = new RossignolContextLocal())
+            using (var context = new RossignolContextLocal(options))
             {
                 context.EncryptedEntriesSet.Remove(e.ToEntity(lue));
                 context.SaveChanges();
             }
         }
 
-        //public async Task<EncryptedProprietaryEntry> GetProprietaryEntries
+        public static async Task clearDB(DbContextOptions<RossignolContextLocal> options)
+        {
+            using (var context = new RossignolContextLocal(options))
+            {
+                context.EncryptedEntriesSet.RemoveRange(context.EncryptedEntriesSet);
+                context.SaveChanges();
+            }
+        }
 
     }
 }

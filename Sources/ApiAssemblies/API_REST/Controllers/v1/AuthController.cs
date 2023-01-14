49d0fc46b4a491673b9219b03e3fe7a7514ce67e
yorick.geoffre <yorick.geoffre@etu.uca.fr>
2023-01-14 09:41:57 +0100

    Annotations done

--- Sources/ApiAssemblies/API_REST/Controllers/v1/AuthController.cs
+++ Sources/ApiAssemblies/API_REST/Controllers/v1/AuthController.cs
@@ -3,10 +3,12 @@
 namespace API_REST.Controllers.V1
 {
     [ApiController]
+    [ApiVersion("1.0")]
     [Route("/api/v{version:apiVersion}/[controller]")]
     public class AuthController : ControllerBase
     {
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status501NotImplemented)]
         public IActionResult GetAuthToken(string email, string password)
         {
             return StatusCode(501);

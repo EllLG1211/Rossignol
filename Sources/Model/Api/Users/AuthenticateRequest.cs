93fc3ae7fcfb2e590fb7aae0d13b5f1b8385aea4
Valentin Clergue <valentin.clergue@etu.uca.fr>
2023-01-28 12:04:43 +0100

    add model classes for api

---
+++ Sources/Model/Api/Users/AuthenticateRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Model.Api.Users
+{
+    public class AuthenticateRequest
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}

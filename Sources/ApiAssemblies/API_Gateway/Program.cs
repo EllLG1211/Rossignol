ab40ec6830d40d3f9fa8fb5812af06689af8f2b1
Mathis Ribemont <mathis.ribemont@etu.uca.fr>
2023-02-11 11:09:10 +0100

    #129 add auth field in swagger

--- Sources/ApiAssemblies/API_Gateway/Program.cs
+++ Sources/ApiAssemblies/API_Gateway/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Data;
+using Microsoft.OpenApi.Models;
 using Model.Business;
 using Ocelot.Middleware;
 
@@ -48,6 +49,32 @@ if (builder.Environment.EnvironmentName == "Development")
 {
     builder.Services.AddSwaggerGen(options =>
     {
+        options.AddSecurityDefinition(name: "Bearer", securityScheme: new OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+            Description = "Enter the Bearer Authorization string as following: `Bearer Generated-JWT-Token`",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer"
+        });
+        options.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Name = "Bearer",
+                    In = ParameterLocation.Header,
+                    Reference = new OpenApiReference
+                    {
+                        Id = "Bearer",
+                        Type = ReferenceType.SecurityScheme
+                    }
+                },
+                new List<string>()
+            }
+        });
+
+        
         var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
         options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
     });

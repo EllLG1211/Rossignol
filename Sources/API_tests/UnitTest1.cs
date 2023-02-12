ed76def0838cb4c4908cf4fcf201137c61424a49
yorick.geoffre <yorick.geoffre@etu.uca.fr>
2023-02-12 23:17:35 +0100

    utest

--- Sources/API_tests/UnitTest1.cs
+++ Sources/API_tests/UnitTest1.cs
@@ -2,7 +2,9 @@ using API_Gateway.Controllers;
 using API_Gateway.Helpers;
 using API_Gateway.Services;
 using API_REST.Controllers.V1;
+using DTOs;
 using EF_Local.Managers;
+using EF_Model.Utils;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -62,8 +64,12 @@ namespace API_tests
         [Fact]
         public void testREST()
         {
+            List<ConnectedUser> data = makeTestUsers();
+
             IDataManager idm = new EFDataManager(":memory:");
             ILogger<EntriesController> il = new blendLogger<EntriesController>();
+            ILogger<AccountsController> ila = new blendLogger<AccountsController>();
+
             ILogger<JwtUtils> ilj = new blendLogger<JwtUtils>();
             var appset = new AppSettings()
             {
@@ -73,6 +79,14 @@ namespace API_tests
             IJwtUtils jwt = new JwtUtils(ioa, ilj);
             AutoMapper.IMapper ima = null;
             EntriesController ec = new EntriesController(il,ima, idm, jwt);
+            AccountsController ac = new AccountsController(ila, ima, idm);
+
+            AccountDTO adt = new AccountDTO();
+            adt.Mail = data.First().Mail;
+            adt.Password = data.First().Password;
+            //ac.AddUser(adt);
+            
+            //ec.List(jwt.GenerateJwtToken(new Model.Api.Entities.User(data.First().Uid, data.First().Mail)));
         }
 
         private List<ConnectedUser> makeTestUsers()
